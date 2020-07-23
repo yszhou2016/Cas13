05c805590510e6515be8c743b73b1a2d529432e6
yszhou2016 <416256889@qq.com>
2020-07-23 16:11:18 +0800

    Create 1.Cuffdiff.sh

---
+++ 1.RNA-OffTarget/1.Cuffdiff.sh
@@ -0,0 +1,1 @@
+cuffdiff   -p 18 -o  Control_Treat  ~/data/hg19//Homo_sapiens.GRCh38.96.chr.gtf     $sample1.Rep1.sorted.bam,$sample1.Rep2.sorted.bam,$sample1.Rep3.sorted.bam    $sample2.Rep1.sorted.bam,$sample2.Rep2.sorted.bam,$sample2.Rep3.sorted.bam 
