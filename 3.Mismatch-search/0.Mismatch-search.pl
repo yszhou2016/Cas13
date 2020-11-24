959bddf0d0c9ed9bbb549fd2bc8ed5b12c2f6b88
yszhou2016 <416256889@qq.com>
2020-11-24 14:29:41 +0800

    Create 0.Mismatch-search.pl

---
+++ 3.Mismatch-search/0.Mismatch-search.pl
@@ -0,0 +1,46 @@
+$file_input=$ARGV[0];    ###  fasta (fa） file of spacer sequence
+$refgenome=$AARGV[1];       ###  fasta (fa) file of refgenome
+$mismatch=$ARGV[2];             ###  max mismatch between spacer and target 
+#########################
+open(AA,"$file_input");
+while($line=<AA>)
+{   $line2=<AA>;  
+    chomp $line; $line=~s/>//;  @tt=split/\s+/,$line;  $target=$tt[0];
+    chomp $line2;
+    @bb=split//,$line2;
+    $len=@bb;
+    $s=0;
+  #######################################
+  open(FF,"$refgenome");
+  open(BB,">$target.fa");
+  while($lin=<FF>)
+  { $lin2=<FF>;
+    chomp $lin;  $lin=~s/>//;  @ar=split/\s+/,$lin;
+    chomp $lin2;
+    $len2=length($lin2);
+    $i=0;
+    while($i<$len2)
+      {
+        $seq=substr($lin2,$i,$len+3);
+        $seq2=$seq;   $seq2=~tr/ATCGatcg/TAGCtagc/;  $seq2=reverse($seq2);
+        @bb2=split//,$seq;
+        @bb3=split//,$seq2;
+        $j=0;  $t1=0;  $t2=0;
+        while($j<$len)
+            {  if($bb[$j] eq $bb2[$j]){$t1++;}
+               if($bb[$j] eq $bb3[$j]){$t2++;}
+               $j++;
+            }
+            $mismatch1=$len-$t1;
+            $mismatch2=$len-$t2;  
+            if($mismatch1<=$mismatch){$s++;  print BB ">$target-$s\t$ar[0]\t$i\t+\t$mismatch1\n$seq\n";}
+            if($mismatch2<=$mismatch){$s++;  print BB ">$target-$s\t$ar[0]\t$i\t-\t$mismatch2\n$seq2\n";}
+            #if($mismatch<=10){print $seq,"\n";}    
+        $i++;
+      }
+    
+  }
+  close FF;  close BB;
+  #########################################
+}
+close AA;  
