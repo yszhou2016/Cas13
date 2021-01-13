8e2159fea6e7175655e969b4ba816397b27ba715
yszhou2016 <416256889@qq.com>
2021-01-13 21:11:12 +0800

    Update 0.Cas-Finder.pl
    1. setup the “prodigal.linux” , "bedtools" and “pilercr” in the default environment.
    bedtools:  https://sourceforge.net/projects/bedtools/
    pilecr:  http://www.drive5.com/pilercr/
    
    2. run “perl  0.Cas-Finder  $sample.fasta ”

--- 0.Cas-Finder/0.Cas-Finder.pl
+++ 0.Cas-Finder/0.Cas-Finder.pl
@@ -1,7 +1,7 @@
-$file=$ARGV[0];  
+$file=$ARGV[0];    # input a fasta file of genome/metagenome
 ################ 
 ## 1. setup “prodigal.linux” , "bedtools" and “pilercr” in the default environment
-## 2. run “perl  0.Cas-Finder  $sample.fasta ”
+## 2. example:  perl  0.Cas-Finder  $sample.fasta 
 ################ 
 
 
