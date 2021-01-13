2108e4076145f8936c5270d583b0f9ad15e4722e
yszhou2016 <416256889@qq.com>
2021-01-13 16:54:10 +0800

    Update Mismatch-search.pl

--- 3.Mismatch-search/Mismatch-search.pl
+++ 3.Mismatch-search/Mismatch-search.pl
@@ -1,8 +1,25 @@
-$file_input=$ARGV[0];    ###  fasta (fa） file of spacer sequence
-$refgenome=$AARGV[1];       ###  fasta (fa) file of refgenome
-$mismatch=$ARGV[2];             ###  max mismatch between spacer and target 
-#########################
-open(AA,"$file_input");
+$spacer=$ARGV[0];                    #  fasta file of query sequences
+$reference=$ARGV[1];                 #  fasta file of reference genome for search
+$MaxMismatch=$ARGV[2];               #  max mismatch between spacer and target 
+###################################################
+#  Example:  perl   Mismatch-search.pl     Spacer.fa    Hg38.genome.fa    8
+###################################################
+open(FASTA,"$reference");
+open(FA,">genome.tmp");
+$s=0;
+while($line=<FASTA>)
+{  chomp $line;
+   if($line=~/^>/)
+   { if($s>0){print FA "\n";}
+       print FA $line,"\n";
+     $s=1;
+   } else{  $line=~tr/atcg/ATCG/;  print FA $line;}
+ } 
+ print FA "\n";
+close FASTA; close FA;
+
+##################################
+open(AA,"$spacer");
 while($line=<AA>)
 {   $line2=<AA>;  
     chomp $line; $line=~s/>//;  @tt=split/\s+/,$line;  $target=$tt[0];
@@ -10,11 +27,11 @@ while($line=<AA>)
     @bb=split//,$line2;
     $len=@bb;
     $s=0;
-  #######################################
-  open(FF,"$refgenome");
+  ##############
+  open(FF,"genome.tmp");
   open(BB,">$target.fa");
   while($lin=<FF>)
-  { $lin2=<FF>;
+  { $lin2=<FF>;  
     chomp $lin;  $lin=~s/>//;  @ar=split/\s+/,$lin;
     chomp $lin2;
     $len2=length($lin2);
@@ -22,7 +39,7 @@ while($line=<AA>)
     while($i<$len2)
       {
         $seq=substr($lin2,$i,$len+3);
-        $seq2=$seq;   $seq2=~tr/ATCGatcg/TAGCtagc/;  $seq2=reverse($seq2);
+        $seq2=$seq;   $seq2=~tr/ATCGatcg/TAGCTAGC/;  $seq2=reverse($seq2);
         @bb2=split//,$seq;
         @bb3=split//,$seq2;
         $j=0;  $t1=0;  $t2=0;
@@ -33,9 +50,8 @@ while($line=<AA>)
             }
             $mismatch1=$len-$t1;
             $mismatch2=$len-$t2;  
-            if($mismatch1<=$mismatch){$s++;  print BB ">$target-$s\t$ar[0]\t$i\t+\t$mismatch1\n$seq\n";}
-            if($mismatch2<=$mismatch){$s++;  print BB ">$target-$s\t$ar[0]\t$i\t-\t$mismatch2\n$seq2\n";}
-            #if($mismatch<=10){print $seq,"\n";}    
+            if($mismatch1<=$MaxMismatch){$s++;  print BB ">$target-$s\t$ar[0]\t$i\t+\t$mismatch1\n$seq\n";}
+            if($mismatch2<=$MaxMismatch){$s++;  print BB ">$target-$s\t$ar[0]\t$i\t-\t$mismatch2\n$seq2\n";}
         $i++;
       }
     
@@ -44,3 +60,4 @@ while($line=<AA>)
   #########################################
 }
 close AA;  
+
